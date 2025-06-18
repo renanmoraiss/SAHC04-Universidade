fb74d29e792d0a13dc7e44ec5aeec9799f5f5ade
moraissdev <renanbusinessdev@gmail.com>
2025-06-18 20:46:22 -0300

    commit

--- exercicioA.c
+++ exercicioA.c
@@ -22,7 +22,7 @@ void entrada(int *numeroDeFilhos) {
 void soma(int numeroDeFilhos) {
     int soma = 0;
     int F;
-    for (int i = 0; i < numeroDeFilhos; i++) {
+    for (int i = 1; i <= numeroDeFilhos; i++) {
         scanf("%d", &F);
         if (F >= 0 && F <= 1000) {
             soma += F;
